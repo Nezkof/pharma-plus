1ae35a0f609867dff61834ea227e8a37f096465f
Yaroslav <yaroslavkoshylko@gmail.com>
2025-03-08 22:00:38 +0200

    feat: cart page

--- src/scripts/pages/products.js
+++ src/scripts/pages/products.js
@@ -1,5 +1,7 @@
+import LocationMenuCollection from "../components/LocationMenu.ts";
 import FilteringMenuCollection from "../components/FilteringMenu.ts";
 import FiltersBarCollection from "../components/FiltersBar.ts";
 
+new LocationMenuCollection();
 new FilteringMenuCollection();
 new FiltersBarCollection();
